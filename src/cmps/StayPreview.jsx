0447d19ddfb352c9a9a106afd06b81fc6cfb655a
Adi <12345678+username@users.noreply.github.com>
2025-08-24 22:41:16 +0300

    Add link

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -10,7 +10,7 @@ export function StayPreview({ stay }) {
     return <article className="stay-preview">
         {/* <img className="stay-img" src={stay.imgUrls?.[0]} alt="" /> */}
         <ImgCarousel imgUrls={stay.imgUrls} stayName={stay.name} />
-        {/* <Link to={`/stay/${stay._id}`}>{stay.name}</Link> */}
+        <Link to={`/stay/${stay._id}`}>{stay.name}</Link>
 
         <div class="preview-info">
             <div class="preview-name bold">{stay.name}</div>
