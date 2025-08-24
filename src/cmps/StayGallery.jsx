8860130ea48fb488d526be7c6fbd858533eb10d4
Itaish-Bzu <Itaish94@gmail.com>
2025-08-24 19:20:22 +0300

    Add photos

--- src/cmps/StayGallery.jsx
+++ src/cmps/StayGallery.jsx
@@ -1,15 +1,14 @@
 export function StayGallery() {
   const images = [
-    'https://robohash.org/robot1?set=set1',
-    'https://robohash.org/robot2?set=set1',
-    'https://robohash.org/robot3?set=set1',
-    'https://robohash.org/robot4?set=set1',
-    'https://robohash.org/robot5?set=set1',
-    'https://robohash.org/robot6?set=set1',
-    'https://robohash.org/robot7?set=set1',
-    'https://robohash.org/robot8?set=set1',
-    'https://robohash.org/robot9?set=set1',
-    'https://robohash.org/robot10?set=set1',
+    'https://robohash.org/0?set=set5',
+    'https://robohash.org/1?set=set5',
+    'https://robohash.org/2?set=set5',
+    'https://robohash.org/3?set=set5',
+    'https://robohash.org/4?set=set5',
+    'https://robohash.org/5?set=set5',
+    'https://robohash.org/6?set=set5',
+    'https://robohash.org/7?set=set5',
+    'https://robohash.org/8?set=set5',
   ]
 
   return (
@@ -20,10 +19,13 @@ export function StayGallery() {
         <button>save</button>
       </div>
       <div className='gallery'>
-        {images.map((Image) => {
-          <article>
-            <img src={Image} alt='photo in gallery' />
-          </article>
+        {images.map((Image, idx) => {
+          if (idx < 5)
+            return (
+              <article key={Image}>
+                <img src={Image} alt='photo in gallery' />
+              </article>
+            )
         })}
       </div>
     </section>
