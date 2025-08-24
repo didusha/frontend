fec1d8b7037a423965fc833e9e58be0229ec0d45
Adi <12345678+username@users.noreply.github.com>
2025-08-24 15:20:01 +0300

    Test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ export function HomePage() {
     return (
         <section className="home">
             <h1>Home sweet Home</h1>
-            <h1>Home sweet Home</h1>
         </section >
     )
 }
