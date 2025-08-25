12c11078222d88d5f960538c31aa088631a760d0
Adi <12345678+username@users.noreply.github.com>
2025-08-25 13:39:21 +0300

    stay list & preview CSS update

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -2,41 +2,28 @@ import { Link } from 'react-router-dom'
 import { ImgCarousel } from './ImgCarousel.jsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { formatDateRange } from '../services/util.service.js';
 
 
 export function StayPreview({ stay }) {
-
-    function formatDateRange(startDateStr, endDateStr) {
-        const start = new Date(startDateStr)
-        const end = new Date(endDateStr)
-
-        const options = { month: 'short', day: 'numeric' }
-
-        // If the month is the same, only show month once
-        if (start.getMonth() === end.getMonth()) {
-            return `${start.toLocaleDateString('en-US', { month: 'short' })} ${start.getDate()}-${end.getDate()}`
-        } else {
-            return `${start.toLocaleDateString('en-US', options)} - ${end.toLocaleDateString('en-US', options)}`
-        }
-    }
 
     return <Link to={`/stay/${stay._id}`}>
         <article className="stay-preview">
             <ImgCarousel imgUrls={stay.imgUrls} stayName={stay.name} />
 
             <div className="preview-info">
-                <div className="preview-name bold">{stay.name}</div>
-                <span className="preview-data-info flex">
-                    <span className="preview-rate-icon">
-                        <FontAwesomeIcon icon={faStar} />
-                    </span>
-                    <span className="preview-rate bold">{stay.rating}</span>
+                <div className="preview-name ">{stay.name.charAt(0).toUpperCase() + stay.name.slice(1).toLowerCase()}</div>
+                <span className="preview-data-info">
+                    <span className="preview-rate">
+                        <span><FontAwesomeIcon icon={faStar} /></span>
+                        {stay.rating} 4.88(15)
+                        </span>
                 </span>
-                <div className="preview-stay-with grey">Stay with {stay.host.fullname} - Host for {9} years</div>
+                <div className="preview-stay-with grey">Stay with {stay.host.fullname} • Host for {9} years</div>
                 {/* <div className=""></div> */}
                 <div className="preview-dates grey">{formatDateRange(stay.startDate, stay.endDate)}</div>
                 <div className="preview-price">
-                    <span className="bold">${stay.price}</span><span className="grey"> night</span>
+                    <span className="">$ {stay.price}</span><span className="grey"> night</span>
                 </div>
             </div>
         </article>
