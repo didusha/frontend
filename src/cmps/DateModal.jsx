30a8d0ece081f188a9a5633439a684ea7081bf04
avihayrevivo <avihayr5@gmail.com>
2025-08-25 19:35:30 +0300

    Make new modal

--- src/cmps/DateModal.jsx
+++ src/cmps/DateModal.jsx
@@ -1,56 +1,50 @@
 import { useState } from 'react'
 import Modal from 'react-modal'
-import { DateRange } from 'react-date-range'
-import 'react-date-range/dist/styles.css'
-import 'react-date-range/dist/theme/default.css'
+
+import { DayPicker } from 'react-day-picker'
+import 'react-day-picker/dist/style.css'
 
 Modal.setAppElement('#root')
 
 export function DateModal({ isOpen, onClose, setCheckInDate, setCheckOutDate }) {
-    const [range, setRange] = useState([
-        {
-            startDate: new Date(),
-            endDate: null,
-            key: 'selection'
-        }
-    ])
+    const [range, setRange] = useState({ from: null, to: null })
+    // [
+    // {
+    //     startDate: new Date(),
+    //     endDate: null,
+    //     key: 'selection'
+    // }
+    // ])
 
     return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onClose}
-            contentLabel="Select Date"
-            style={{
-                overlay: {
-                    backgroundColor: 'transparent',
-                },
-                content: {
-                    maxWidth: '750px',
-                    margin: 'auto',
-                    padding: '1em',
-                    borderRadius: '12px',
-                    height: '550px'
-                },
-            }}
-        >
-            <div className="airbnb-calendar">
-                <DateRange
-                    editableDateInputs={true}
-                    onChange={item => {
-                        const newRange = [item.selection]
-                        setRange(newRange)
-                        setCheckInDate(newRange[0].startDate)
-                        setCheckOutDate(newRange[0].endDate)
-                    }}
-                    moveRangeOnFirstSelection={false}
-                    ranges={range}
-                    months={2}
-                    direction="horizontal"
-                    rangeColors={['#000']}
-                    showMonthAndYearPickers={false}
-                    weekdayDisplayFormat="EEEEE"
-                />
-            </div>
-        </Modal>
+        <div className="modal-calendar">
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={onClose}
+                contentLabel="Select Date"
+                style={{
+                    overlay: {
+                        backgroundColor: 'transparent',
+                    },
+                    content: {
+                        maxWidth: '750px',
+                        margin: 'auto',
+                        padding: '1em',
+                        borderRadius: '12px',
+                        height: '400px',
+                    },
+                }}
+            >
+                <div className="airbnb-calendar">
+                    <DayPicker
+                        mode="range"
+                        selected={range}
+                        onSelect={setRange}
+                        numberOfMonths={2}
+                        pagedNavigation
+                    />
+                </div>
+            </Modal>
+        </div>
     )
 }
