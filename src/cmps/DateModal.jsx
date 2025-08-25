4958a2407ec0ea93f75e0679ee8674ae17fbd525
avihayrevivo <avihayr5@gmail.com>
2025-08-25 12:12:05 +0300

    Make header sticky

--- src/cmps/DateModal.jsx
+++ src/cmps/DateModal.jsx
@@ -16,36 +16,36 @@ export function DateModal({ isOpen, onClose }) {
     ])
 
     return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onClose}
-            contentLabel="Select Date"
-            style={{
-                overlay: {
-                    backgroundColor: 'transparent', 
-                },
-                content: {
-                    maxWidth: '750px',
-                    margin: 'auto',
-                    padding: '1em',
-                    borderRadius: '12px',
-                    height: '550px'
-                },
-            }}
-        >
-            <div className="airbnb-calendar">
-                <DateRange
-                    editableDateInputs={true}
-                    onChange={item => setRange([item.selection])}
-                    moveRangeOnFirstSelection={false}
-                    ranges={range}
-                    months={2}
-                    direction="horizontal"
-                    rangeColors={['#000']}
-                    showMonthAndYearPickers={false}
-                    weekdayDisplayFormat="EEEEE"
-                />
-            </div>
-        </Modal>
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={onClose}
+                contentLabel="Select Date"
+                style={{
+                    overlay: {
+                        backgroundColor: 'transparent',
+                    },
+                    content: {
+                        maxWidth: '750px',
+                        margin: 'auto',
+                        padding: '1em',
+                        borderRadius: '12px',
+                        height: '550px'
+                    },
+                }}
+            >
+                <div className="airbnb-calendar">
+                    <DateRange
+                        editableDateInputs={true}
+                        onChange={item => setRange([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={range}
+                        months={2}
+                        direction="horizontal"
+                        rangeColors={['#000']}
+                        showMonthAndYearPickers={false}
+                        weekdayDisplayFormat="EEEEE"
+                    />
+                </div>
+            </Modal>
     )
 }
