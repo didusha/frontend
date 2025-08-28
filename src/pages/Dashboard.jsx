60b1c525aa6a5594dcdbbd1ba71e63bda96f5011
Itaish-Bzu <Itaish94@gmail.com>
2025-08-28 20:33:59 +0300

    Add images

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,55 @@
+import { useEffect, useState } from 'react'
+import { orderService } from '../services/order/order.service.local'
 
-
-export function Dashboard(){
-
-
-    return(
-        <div>dashboard</div>
-    )
+export function Dashboard() {
+  const [orders, setOrders] = useState()
+  
+  
+  useEffect(() => {
+      loadOrders()
+    }, [])
+    
+    async function loadOrders() {
+        const orders = await orderService.query()
+        setOrders(orders)
+        
+    }
+    
+    
+    if (!orders) return <div>loading</div>
+    console.log(orders);
+  return (
+    <section>
+        <h2>{orders.length} Reservations</h2>
+    <table>
+        <tr>
+            <th>Guest</th>
+            <th>Check-in</th>
+            <th>Check-out</th>
+            <th>Booked</th>
+            <th>Total Payout </th>
+            <th>Status</th>
+        </tr>
+        {orders.map(order=>{
+            
+        <tr key={order._id}>
+            <td>{order.guest}</td>
+            <td>{order.startDate}</td>
+            <td>19</td>
+            <td>Male</td>
+        </tr>
+        })}
+        <tr>
+            <td>Megha</td>
+            <td>19</td>
+            <td>Female</td>
+        </tr>
+        <tr>
+            <td>Subham</td>
+            <td>25</td>
+            <td>Male</td>
+        </tr>
+    </table>
+    </section>
+  )
 }
