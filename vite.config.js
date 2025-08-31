8a8f1f6de47fab9d5b9abf308ac3bfbe21afac3c
Adi <12345678+username@users.noreply.github.com>
2025-08-31 21:11:41 +0300

    Add build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: '../backend/public',
+		emptyOutDir: true,
+	}
+
 	// After setting-up a backend, this can automoate the copying process of the built files:
 	// build: {
 	// 	outDir: '../backend/public',
